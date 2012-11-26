2a4e74550dc391af69d33e88f9a2a197559bf18b
Mibias <Mibias.madsen@gmail.com>
2012-11-26 21:30:37 +0100

    Test af eclipse plugin

--- GPP-Eksamensprojekt/src/logic/Main.java
+++ GPP-Eksamensprojekt/src/logic/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("It works?");
 		
-		System.out.println("It works!");
+		System.out.println("No it doesn't");
 
 	}
 
