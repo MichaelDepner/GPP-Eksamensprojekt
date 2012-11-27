8ecbdc3e1ed8b4c68ff86d41e4293f6b09b8fa1b
Michael Frikke Madsen <mibias.madsen@gmail.com>
2012-11-27 13:45:02 +0100

    Tilføjer Forside prototype

--- GPP-Eksamensprojekt/src/gui/Forside.java
+++ GPP-Eksamensprojekt/src/gui/Forside.java
@@ -1,5 +1,78 @@
 package gui;
 
-public class Forside {
+import java.awt.*;
 
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class Forside {
+	private JFrame frame;
+	private JPanel southPanel = new JPanel();
+	private JPanel centerPanel = new JPanel();
+	private JPanel northPanel = new JPanel();
+	private JButton rejse, booking, afgang;
+	
+	private JLabel logoLabel;
+	
+	
+	
+	public Forside() {
+		makeWindow();
+	}
+	
+	private void makeWindow() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		
+		
+		Container contentPane = frame.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		centerPanel.setLayout(new GridLayout(3, 1));
+		
+		ImageIcon imageLogo = new ImageIcon(getClass().getResource("png/Logo.png"));
+		logoLabel = new JLabel(imageLogo);
+		
+		contentPane.add(centerPanel, BorderLayout.CENTER);
+		contentPane.add(logoLabel, BorderLayout.NORTH);
+		
+		rejse = new JButton("Søg rejser");
+		booking = new JButton("Søg booking");
+		afgang = new JButton("Søg afgang");
+		
+		rejse.addActionListener(new rejseActionListener());
+		booking.addActionListener(new bookingActionListener());
+		afgang.addActionListener(new afgangActionListener());
+		
+		centerPanel.add(rejse);
+		centerPanel.add(afgang);
+		centerPanel.add(booking);
+		
+		frame.setPreferredSize(new Dimension(640, 460));
+		frame.setResizable(false);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	private class rejseActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Søger rejser!");
+		}
+	}
+	
+	private class afgangActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Søger afgange!");
+		}
+	}
+	
+	private class bookingActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Søger bookingner!");
+		}
+	}
+	
+	
+	
 }
