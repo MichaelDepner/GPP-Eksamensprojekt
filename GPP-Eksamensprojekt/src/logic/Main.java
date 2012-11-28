4e7925365c627c2039e85dc20cce2e850160ab4c
Mibias <Mibias.madsen@gmail.com>
2012-11-28 20:46:17 +0100

    Begynder på Database klassen.

--- GPP-Eksamensprojekt/src/logic/Main.java
+++ GPP-Eksamensprojekt/src/logic/Main.java
@@ -1,12 +1,20 @@
 package logic;
 
+import java.sql.SQLException;
+
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		new gui.Forside();
+		
+		Database db = new Database("mysql.itu.dk", "Swan_Airlines", "swan", "mintai");
+		db.queryGetAirports();
+		db.close();
+		
 		}
 	}
