31a2a18a9acf88303e68666bab18761992d97e53
Tajanna92 <tajanna@tbkj.dk>
2012-11-28 21:06:15 +0100

    Faner under Afgangsliste

--- GPP-Eksamensprojekt/src/gui/Afgangsliste.java
+++ GPP-Eksamensprojekt/src/gui/Afgangsliste.java
@@ -1,5 +1,41 @@
 package gui;
 
-public class Afgangsliste {
+import javax.swing.*;
 
-}
+public class Afgangsliste extends JFrame {
+	
+	    public Afgangsliste() {
+	        
+	        setTitle("Afgange");
+	        JTabbedPane jtp = new JTabbedPane();
+	        getContentPane().add(jtp);
+	        
+	        JPanel jp1 = new JPanel();
+	        JPanel jp2 = new JPanel();
+	        JPanel jp3 = new JPanel();
+	        
+	        JLabel label1 = new JLabel();
+	        label1.setText("Onsdag d. 28. november 2012");
+	        JLabel label2 = new JLabel();
+	        label2.setText("Torsdag d. 29. november 2012");
+	        JLabel label3 = new JLabel();
+	        label3.setText("Fredag d. 30. november 2012");
+	        
+	        jp1.add(label1);
+	        jp2.add(label2);
+	        jp3.add(label3);
+	        
+	        jtp.addTab("28/11", jp1);
+	        jtp.addTab("29/11", jp2);
+	        jtp.addTab("30/11", jp3);
+	        
+	    }
+	    public static void main(String[] args) {
+	        
+	        Afgangsliste tp = new Afgangsliste();
+	        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        tp.setVisible(true);
+	        
+	    }
+	}
+
