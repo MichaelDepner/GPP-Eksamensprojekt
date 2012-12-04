a9945266ac9251457a5f59e555beabf8cd232f14
Elayoe <nicoline.larsen@webgolf.dk>
2012-12-04 14:18:02 +0100

    ?

--- GPP-Eksamensprojekt/src/gui/Forside2.java
+++ GPP-Eksamensprojekt/src/gui/Forside2.java
@@ -40,7 +40,7 @@ public class Forside2  extends JFrame implements ActionListener{
 		panelAfgange = new JPanel();
 		panelBooking = new JPanel();
 		
-		//Tilføjer faner
+		//Tilføjer faner med tilhørende paneler
 		searchPane.addTab("Søg rejser", panelRejser);
 		searchPane.addTab("Søg afgange", panelAfgange);
 		searchPane.addTab("Søg bookinger", panelBooking);
@@ -51,6 +51,7 @@ public class Forside2  extends JFrame implements ActionListener{
 		//laver hele fanevinduet til at søge rejser
 		searchRejser();
 		addHjemrejse();
+		searchBookinger();
 		
 		panel.setPreferredSize(new Dimension(700, 550));
 		//setResizable(false);
@@ -169,10 +170,6 @@ public class Forside2  extends JFrame implements ActionListener{
 	    udrejseDate = new JXDatePicker();
 	    panelLeft.add(udrejseDate);
 	    
-	    
-	    
-	    
-	    
 	    //Laver et panel, der sættes ind i panel SOUTH
   		panelSouth = new JPanel();
   		panelRejser.add(panelSouth, BorderLayout.SOUTH);
@@ -189,6 +186,7 @@ public class Forside2  extends JFrame implements ActionListener{
 	}
 	
 	private void searchBookinger() {
-		
+		//Laver et BorderLayout inde i fane-vinduet bookinger
+		panelBooking.setLayout(new BorderLayout(10,10));
 	}
 }
